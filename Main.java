2ee6adfe882fc253cd8bd980a1142ac9392c8333
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2022-09-14 12:21:39 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]) {
+        String[] words = { "Zero", "Gwambus", "Robert", "Mwamba", "Armmy", "Agronomy" };
 
+        System.out.println("Presorted Array: ");
+        System.out.println();
+        for (String i : words) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+        Arrays.sort(words);
+        System.out.println("Sorted Array: ");
+        System.out.println();
+        for (String i : words) {
+            System.out.print(i + " ");
+        }
     }
 }
