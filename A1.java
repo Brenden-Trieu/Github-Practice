a096d412ea033804db80fc8298c8ee88e74a1e82
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2022-09-14 12:10:39 -0700

    Update A1.java

--- A1.java
+++ A1.java
@@ -11,10 +11,19 @@ public class A1 {
             System.out.print(num + " ");
         }
         System.out.println();
+        for (String i : words) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
         Arrays.sort(arr);
+        Arrays.sort(words);
         System.out.println("Sorted Array: ");
         for (int num : arr) {
             System.out.print(num + " ");
+        }
+        System.out.println();
+        for (String i : words) {
+            System.out.print(i + " ");
         }
     }
 }
