b4c74b53b486afeeca7967d22311abe3f3375393
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2022-09-14 12:48:16 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,30 +5,24 @@ public class Main {
         String[] words = { "Zero", "Gwambus", "Robert", "Mwamba", "Armmy", "Agronomy" };
         int[] arr = { 15, 57, 4, 64, 11, 20, 41, 3 };
 
-        System.out.println("Presorted Array: ");
+        System.out.println("\nPresorted Array: ");
         for (int num : arr) {
             System.out.print(num + " ");
         }
         System.out.println();
-        System.out.println("\n");
+        for (String i : words) {
+            System.out.print(i + " ");
+        }
         Arrays.sort(arr);
-        System.out.println("Sorted Array: ");
+        Arrays.sort(words);
+        System.out.println("\n\nSorted Array: ");
         for (int num : arr) {
             System.out.print(num + " ");
         }
         System.out.println();
-
-        System.out.println("Presorted Array: ");
-        System.out.println();
         for (String i : words) {
             System.out.print(i + " ");
         }
-        System.out.println("\n");
-        Arrays.sort(words);
-        System.out.println("Sorted Array: ");
         System.out.println();
-        for (String i : words) {
-            System.out.print(i + " ");
-        }
     }
 }
