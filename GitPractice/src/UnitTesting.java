afbd3f382cc39fc30437a480f2672375a0af82db
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2022-09-16 12:26:46 -0700

    Refactor

---
+++ GitPractice/src/UnitTesting.java
@@ -0,0 +1,30 @@
+import org.junit.*;
+import org.junit.internal.ArrayComparisonFailure;
+import org.junit.internal.ExactComparisonCriteria;
+
+public class UnitTesting {
+    @Test
+    public void NumberTest() {
+        int[] tester = { 19, 5, 44, 48, 1, 88, 50, 37 };
+        int[] result = { 1, 5, 19, 37, 44, 48, 50, 88 };
+        App.sortNum(tester);
+        assertArrayEquals(result, tester);
+        return;
+    }
+
+    public void StringTest() {
+        String[] wordsArr = { "Ricky", "Arson", "Punk", "Zipper", "Mary" };
+        String[] resultStrings = { "Arson", "Mary", "Punk", "Ricky", "Zipper" };
+        App.sortString(wordsArr);
+        Assert.assertArrayEquals(resultStrings, wordsArr);
+    }
+
+    public static void assertArrayEquals(int[] expecteds, int[] actuals) throws ArrayComparisonFailure {
+        internalArrayEquals(null, expecteds, actuals);
+    }
+
+    private static void internalArrayEquals(String message, Object expecteds,
+            Object actuals) throws ArrayComparisonFailure {
+        new ExactComparisonCriteria().arrayEquals(message, expecteds, actuals);
+    }
+}
